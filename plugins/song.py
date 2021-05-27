66aa945d3fe7f069328a74dd9deab3774d93fbba
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-05-27 11:19:05 +0530

    🍀🤷‍♂️

--- plugins/song.py
+++ plugins/song.py
@@ -46,7 +46,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[ꜱᴀꜰᴏɴᴇ ᴍᴜꜱɪᴄ]" 
+            performer = f"[© @Mr_D_P]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
